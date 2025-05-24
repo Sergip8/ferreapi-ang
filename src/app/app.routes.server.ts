13b7c6be0ba2fd86ae1bd32e4d724f477699f4dd
sergip8 <sagp758@gmail.com>
2025-05-23 19:35:06 -0500

    route.txt

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,10 +1,46 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { PublicRoutes } from './public/public.routes';
 
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
     renderMode: RenderMode.Prerender
   },
+
+    {
+      path: PublicRoutes.Home,
+    renderMode: RenderMode.Prerender
+    },
+    {
+      path: PublicRoutes.Register,
+     renderMode: RenderMode.Prerender
+    },
+    {
+      path: PublicRoutes.Login,
+      renderMode: RenderMode.Prerender
+    },
+  
+    {
+      path: PublicRoutes.Catalog,
+   renderMode: RenderMode.Prerender
+    },
+     {
+      path: PublicRoutes.Details,
+     renderMode: RenderMode.Prerender
+     
+    },
+    {
+      path: PublicRoutes.Cart,  
+      renderMode: RenderMode.Prerender
+    },
+    {
+      path: PublicRoutes.Contact,
+      renderMode: RenderMode.Prerender
+    },
+    {
+      path: PublicRoutes.About,
+      renderMode: RenderMode.Prerender
+    },
    {
     path: 'details/:id',
     renderMode: RenderMode.Server,
