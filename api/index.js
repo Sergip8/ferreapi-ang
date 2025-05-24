69cd40689b7dcaaa20cf11b4db2457989267a39d
sergip8 <sagp758@gmail.com>
2025-05-23 21:27:58 -0500

    fix

--- api/index.js
+++ api/index.js
@@ -1,3 +1,2 @@
-const server = import('../dist/my-project/server/server.mjs');
-
-module.exports = server.app;
+export default import('../dist/pipe-web/server/server.mjs')
+  .then(module => module.app());
