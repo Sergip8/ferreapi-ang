2f41d3fea11ab31ad83413a81c2634888610b97f
sergip8 <sagp758@gmail.com>
2025-05-23 22:01:51 -0500

    fix url var

--- api/index.js
+++ api/index.js
@@ -1,2 +1,6 @@
-export default import('../dist/pipe-web/server/server.mjs')
-  .then(module => module.app());
+const server = import('../dist/pipe-web/server/server.mjs');
+
+module.exports = server.app;
+
+// export default import('../dist/pipe-web/server/server.mjs')
+//   .then(module => module.app());
--- src/server.ts
+++ src/server.ts
@@ -49,6 +49,7 @@ app.use('/**', (req, res, next) => {
     .catch(next);
 });
 
+
 /**
  * Start the server if this module is the main entry point.
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
