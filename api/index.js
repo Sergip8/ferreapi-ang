c824eb92c22e0b21fca5db78f1880a7fb3d19855
sergip8 <sagp758@gmail.com>
2025-05-24 12:10:33 -0500

    fix

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
-const server = import('../dist/pipe-web/server/server.mjs');
-
-module.exports = server.app;
-
-// export default import('../dist/pipe-web/server/server.mjs')
-//   .then(module => module.app());
+import("../dist/pipe-web/server/server.mjs")
+  .then(module => module.app)
+  .catch(error => {
+    console.error("Failed to load server module:", error);
+    throw error;
+  });
+export default async (req, res) => {
+  const { app } = await import("../dist/pipe-web/server/server.mjs");
+  return app(req, res);
+};
